0decc7b5f9b9f7c5ce39d84aec6f21e2b57e4e5f
kumarayush0104 <kumarayush0104@gmail.com>
2025-07-10 13:49:42 +0530

    ok

---
+++ product_images.py
@@ -0,0 +1,83 @@
+product_images = {
+    "Television": {
+        "Samsung": "product-images/Television/1.png",
+        "LG": "product-images/Television/2.png",
+        "Sony": "product-images/Television/3.png",
+        "Xiaomi": "product-images/Television/4.png",
+        "OnePlus": "product-images/Television/5.png",
+        "TCL": "product-images/Television/6.png",
+        "Vu": "product-images/Television/7.png"
+    },
+    "Refrigerator": {
+        "LG": "product-images/Refrigerator/1.png",
+        "Samsung": "product-images/Refrigerator/2.png",
+        "Whirlpool": "product-images/Refrigerator/3.png",
+        "Godrej": "product-images/Refrigerator/4.png",
+        "Haier": "product-images/Refrigerator/5.png",
+        "Panasonic": "product-images/Refrigerator/6.png"
+    },
+    "Mixer Grinder": {
+        "Philips": "product-images/Mixed-Grinder/1.png",
+        "Bajaj": "product-images/Mixed-Grinder/2.png",
+        "Preethi": "product-images/Mixed-Grinder/3.png",
+        "Morphy Richards": "product-images/Mixed-Grinder/4.png",
+        "Panasonic": "product-images/Mixed-Grinder/5.png",
+        "Havells": "product-images/Mixed-Grinder/6.png"
+    },
+    "Gas Stove": {
+        "Sunflame": "product-images/Gas-Stove/1.png",
+        "Prestige": "product-images/Gas-Stove/2.png",
+        "Pigeon": "product-images/Gas-Stove/3.png",
+        "Butterfly": "product-images/Gas-Stove/4.png",
+        "Bajaj": "product-images/Gas-Stove/5.png",
+        "Elica": "product-images/Gas-Stove/6.png"
+    },
+    "Fans": {
+        "Crompton": "product-images/Fans/1.png",
+        "Orient": "product-images/Fans/2.png",
+        "Havells": "product-images/Fans/3.png",
+        "Bajaj": "product-images/Fans/4.png",
+        "Usha": "product-images/Fans/5.png",
+        "Atomberg": "product-images/Fans/6.png"
+    },
+    "Washing Machine": {
+        "LG": "product-images/Washing_machine/1.png",
+        "Samsung": "product-images/Washing_machine/2.png",
+        "Whirlpool": "product-images/Washing_machine/3.png",
+        "IFB": "product-images/Washing_machine/4.png",
+        "Bosch": "product-images/Washing_machine/5.png",
+        "Godrej": "product-images/Washing_machine/6.png"
+    },
+    "Air Conditioner": {
+        "Daikin": "product-images/AC/1.png",
+        "LG": "product-images/AC/2.png",
+        "Voltas": "product-images/AC/3.png",
+        "Blue Star": "product-images/AC/4.png",
+        "Hitachi": "product-images/AC/5.png",
+        "Samsung": "product-images/AC/6.png"
+    },
+    "Cooler": {
+        "Symphony": "product-images/Cooler/1.png",
+        "Voltas": "product-images/Cooler/2.png",
+        "Bajaj": "product-images/Cooler/3.png",
+        "Kenstar": "product-images/Cooler/4.png",
+        "Crompton": "product-images/Cooler/5.png",
+        "Havells": "product-images/Cooler/6.png"
+    },
+    "Water Purifier": {
+        "Kent": "product-images/Waterpurifier/1.png",
+        "Aquaguard": "product-images/Waterpurifier/2.png",
+        "Pureit": "product-images/Waterpurifier/3.png",
+        "Livpure": "product-images/Waterpurifier/4.png",
+        "Blue Star": "product-images/Waterpurifier/5.png",
+        "HUL": "product-images/Waterpurifier/6.png"
+    },
+    "Geyser": {
+        "Racold": "product-images/Geyser/1.png",
+        "Bajaj": "product-images/Geyser/2.png",
+        "Crompton": "product-images/Geyser/3.png",
+        "AO Smith": "product-images/Geyser/4.png",
+        "Havells": "product-images/Geyser/5.png",
+        "V-Guard": "product-images/Geyser/6.png"
+    }
+}
